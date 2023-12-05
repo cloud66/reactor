2101570f3846539be64c496d62e1e2dd689e6a24
dima <dima@cloud66.com>
2023-12-05 10:38:20 +0000

    Require Reactor::Event after module definition

--- lib/reactor.rb
+++ lib/reactor.rb
@@ -9,7 +9,6 @@ require "reactor/workers/concerns/configuration"
 require "reactor/workers"
 require "reactor/subscription"
 require "reactor/models"
-require "reactor/event"
 
 module Reactor
   SUBSCRIBERS = {}.with_indifferent_access
@@ -63,3 +62,5 @@ module Reactor
     Reactor.rails_6_or_greather? ? :module_parents : :parents
   end
 end
+
+require "reactor/event"
